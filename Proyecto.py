a8259098ad740da574133f2924f53705791ed172
Buenaparte <131205534+Buenaparte@users.noreply.github.com>
2026-03-10 20:50:22 -0400

    Dijstrak
    Se realizo el dijkstra de la menor cantidad de viajes entre dos puntos

--- Proyecto.py
+++ Proyecto.py
@@ -12,6 +12,55 @@ def Guardar_csv():
     #print(Lista_Guardada)
     return Lista_Guardada
 
+def dijkstra_menos_viajes():
+ origen = "POS"
+ destino = "AUA"
+ Lista_Guardada = Guardar_csv()
+ lista = []
+ ruta = []
+ finalizar = False
+ ruta.append(origen)
+ for i in Lista_Guardada:
+        if i[0] == origen:
+         lista.append(i)
+ while True:
+     for i in lista:
+            #print("linea 28")
+            if i[1] == destino:
+             ruta.append(i[1])
+             finalizar = True
+             break
+     if finalizar:
+         break 
+     lista2 = []
+     for i in lista:
+         for j in Lista_Guardada:
+             if j[0] == i[1] or j[1] == i[1]:
+                 #print(j)
+                 if j[1] == destino:
+                        #print("linea 37")
+                        ruta.append(j[0])
+                        ruta.append(j[1])
+                        finalizar = True
+                        break
+                 if j[0] == destino:
+                        #print("linea 37")
+                        ruta.append(j[1])
+                        ruta.append(j[0])
+                        finalizar = True
+                        break 
+                 lista2.append(j)
+     
+         if finalizar:
+             break
+     lista = lista2
+     if finalizar:
+         break
+ print("La ruta con menos viajes es: ")
+ print(ruta)
+
+dijkstra_menos_viajes()
+
 def main():
  Lista_Guardada = Guardar_csv()
  #print(Lista_Guardada)
