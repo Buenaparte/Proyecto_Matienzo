01ae18078a6ec8e404970fa6dee9c061358c33e2
Buenaparte <131205534+Buenaparte@users.noreply.github.com>
2026-03-16 00:41:17 -0400

    visa realizada

--- Proyecto.py
+++ Proyecto.py
@@ -12,45 +12,101 @@ def Guardar_csv():
     #print(Lista_Guardada)
     return Lista_Guardada
 
-def dijkstra_menos_viajes(origen, destino, lista_guardada):
- lista = []
- ruta = []
+def Guardar_visa():
+    Lista_Guardada = []
+    # Abrir el archivo de forma segura
+    with open('Visa.csv', mode='r', encoding='utf-8') as archivo:
+        for fila in archivo:
+         datos = fila.strip().split(',')
+         for i in datos:
+             i = i.split(';')  
+             Lista_Guardada.append(i)  
+             #print(i)
+    Lista_Guardada.pop(0) 
+    #print(Lista_Guardada)
+    return Lista_Guardada
+
+def dijkstra_menos_viajes(origen, destino, lista_guardada, lista_visa, visa ):
+ lista = [] #se guarda los nodos conectados al origen
+ ruta = [] #se guarda la ruta a seguir
  finalizar = False
+ no_se_puede_viajar = False
  ruta.append(origen)
+
+ #se revisa que el destino sea alcanzable sin visa, si no lo es se termina el programa
+ 
+ for i in lista_visa:
+    if i[0] == destino:
+         if visa == False and i[2] == "Requiere Visa":
+             no_se_puede_viajar = True
+             break
+ 
+ if no_se_puede_viajar:
+      return print("No se puede viajar a ese destino sin tarjeta Visa")
+ 
+ #se guarda los nodos conectados al origen en una lista
  for i in lista_guardada:
-        if i[0] == origen:
+        if i[0] == origen or i[1] == origen:
          lista.append(i)
- while True:
-     for i in lista:
+ #print(lista)
+
+ #se revisa si el destino esta conectado al origen, si lo esta se termina el programa
+ for i in lista:
             #print("linea 28")
             if i[1] == destino:
              ruta.append(i[1])
              finalizar = True
-             break
+            if i[0] == destino:
+             ruta.append(i[0])
+             finalizar = True
+ 
+ #se revisa cada uno de los nodos adyacentes al origen
+ while True:
      if finalizar:
-         break 
+         break
+     #print("bucle") 
      lista2 = []
      for i in lista:
-         for j in lista_guardada:
-             if j[0] == i[1] or j[1] == i[1]:
-                 #print(j)
-                 if j[1] == destino:
-                        #print("linea 37")
-                        ruta.append(j[0])
-                        ruta.append(j[1])
-                        finalizar = True
-                        break
-                 if j[0] == destino:
-                        #print("linea 37")
-                        ruta.append(j[1])
-                        ruta.append(j[0])
-                        finalizar = True
-                        break 
-                 lista2.append(j)
-     
+         for k in lista_visa:
+                 #se revisa si el destino es accesible sin visa
+                 if finalizar:
+                     break
+                 if k[0] != origen:
+                     if k[0] == i[1] or k[0] == i[0]  :
+                         print(k)
+                         if visa == False and k[2] == "Requiere Visa":
+                             a = 0
+                         else:
+                             for j in lista_guardada:
+                                 #se revisa si el nodo esta conectado con el nodo adyacente
+                                 if j[0] == i[1] or j[1] == i[1] or j[0] == i[0] or j[1] == i[0]:
+                                     #print(j)
+
+                                     #se revisa si es el nodo destino
+                                     
+                                     if j[1] == destino:
+                                         #print("linea 37")
+                                         ruta.append(j[0])
+                                         ruta.append(j[1])
+                                         finalizar = True
+                                         break
+                                     if j[0] == destino:
+                                         #print(j)
+                                         #print("linea 39")
+                                         ruta.append(j[1])
+                                         ruta.append(j[0])
+                                         finalizar = True
+                                         break 
+                                     
+                                     #se guarda los nodos adyacentes al nodo adyacente
+                                     
+                                     lista2.append(j)
+                                     #print(lista2)
+                         
+                                 if finalizar:
+                                     break
          if finalizar:
              break
-     lista = lista2
      if finalizar:
          break
  print("La ruta con menos viajes es: ")
@@ -58,6 +114,8 @@ def dijkstra_menos_viajes(origen, destino, lista_guardada):
 
 def main():
  Lista_Guardada = Guardar_csv()
+ Lista_Visa = Guardar_visa()
+ visa = False
  #print(Lista_Guardada)
  while True:
      x = input("Coloque el codigo del aeropuerto de origen:  \n")
@@ -84,13 +142,25 @@ def main():
      else:
          print("Codigo no valido")
  while True:
-     z = input("Coloque: \n1- si desea el menor precio posible, \n2- si desea el menor tiempo posible \n")
+     listo = False
+     z = input("Coloque: \n 1- si tiene tarjeta Visa, \n2-  no tiene tarjeta Visa \n") 
+     if z == "1":
+         visa = True
+         listo = True
+     elif z == "2":
+         listo = True
+     else:
+          print("Opcion no valida")
+     if listo:
+          break
+ while True:
+     z = input("Coloque: \n 1- si desea el menor precio posible, \n2- si desea el menor tiempo posible \n")
      listo = False
      if z == "1":
          print("El menor precio posible es: ")
          listo = True
      elif z == "2":
-         dijkstra_menos_viajes(x, y, Lista_Guardada)
+         dijkstra_menos_viajes(x, y, Lista_Guardada, Lista_Visa, visa)
          listo = True
      else:
           print("Opcion no valida")
